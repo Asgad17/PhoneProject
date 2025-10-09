0fc07ff6078b6b8672f6699b66427c85dd4935de
Анара <anaradz72@gmail.com>
2025-10-09 12:33:34 +0600

    added updatePhoneNameById method

--- src/service/PhoneService.java
+++ src/service/PhoneService.java
@@ -5,4 +5,5 @@ import models.Phone;
 public interface PhoneService {
     String addPhone (Phone phone);
     Phone getPhoneById(Long phoneId);
+    Phone updatePhoneNameById(Long phoneId, String newName);
 }
